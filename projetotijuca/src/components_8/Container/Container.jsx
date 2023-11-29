ec427789f22cddb9c5b93d2e0d3593d41e581f10
MuriloXSantos <muriloxsantos@gmail.com>
2023-11-29 16:54:53 -0300

    .

--- projetotijuca/src/components_8/Container/Container.jsx
+++ projetotijuca/src/components_8/Container/Container.jsx
@@ -4,7 +4,20 @@ function Container() {
 
   return (
 
-    <main></main>
+    <main>
+
+        <h1 className="h1_main">Minha lista de desejos</h1>
+
+        <div className="div_quantidade_itens">
+
+            <div className="div_q_i">
+
+                <span className="nome_itens">Itens</span>
+
+                <span className="quantidade_itens">(2)</span>
+            </div>
+        </div>
+    </main>
   )
 }
 
